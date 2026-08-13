a165c4b099ff59d366227e97c57dc0a81b5075dd
Niksh Hiremath <niksh.hiremath24@gmail.com>
2026-08-14 01:50:51 +0530

    Initial configs

---
+++ hypr/modules/monitors.lua
@@ -0,0 +1,11 @@
+------------------
+---- MONITORS ----
+------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({
+    output   = "HDMI-A-4",
+    mode     = "1920x1080@100",
+    position = "auto",
+    scale    = "1",
+})
