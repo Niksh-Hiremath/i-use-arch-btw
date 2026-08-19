fd7ec1eb1368d1b7fc0578e578daab261350ba6e
Niksh Hiremath <niksh.hiremath24@gmail.com>
2026-08-19 19:36:33 +0530

    added floating waybar controls for wifi, audio and bluetooth

--- hypr/modules/rules.lua
+++ hypr/modules/rules.lua
@@ -9,8 +9,8 @@
 
 local suppressMaximizeRule = hl.window_rule({
     -- Ignore maximize requests from all apps. You'll probably like this.
-    name  = "suppress-maximize-events",
-    match = { class = ".*" },
+    name           = "suppress-maximize-events",
+    match          = { class = ".*" },
 
     suppress_event = "maximize",
 })
@@ -18,8 +18,8 @@ local suppressMaximizeRule = hl.window_rule({
 
 hl.window_rule({
     -- Fix some dragging issues with XWayland
-    name  = "fix-xwayland-drags",
-    match = {
+    name     = "fix-xwayland-drags",
+    match    = {
         class      = "^$",
         title      = "^$",
         xwayland   = true,
@@ -31,14 +31,6 @@ hl.window_rule({
     no_focus = true,
 })
 
--- Layer rules also return a handle.
--- local overlayLayerRule = hl.layer_rule({
---     name  = "no-anim-overlay",
---     match = { namespace = "^my-overlay$" },
---     no_anim = true,
--- })
--- overlayLayerRule:set_enabled(false)
-
 -- Hyprland-run windowrule
 hl.window_rule({
     name  = "move-hyprland-run",
@@ -47,3 +39,30 @@ hl.window_rule({
     move  = "20 monitor_h-120",
     float = true,
 })
+
+hl.window_rule {
+    name = "impala",
+    match = { class = "^impala$" },
+    float = true,
+    pin = true,
+    size = { 650, 500 },
+    move = { 1254, 40 }
+}
+
+hl.window_rule {
+    name = "bluetui",
+    match = { class = "^bluetui$" },
+    float = true,
+    pin = true,
+    size = { 500, 450 },
+    move = { 1404, 40 }
+}
+
+function waybar_pavucontrol()
+    hl.dispatch(hl.dsp.exec_cmd("pavucontrol -t 1", {
+        float = true,
+        pin = true,
+        size = { 600, 650 },
+        move = { 1304, 40 }
+    }))
+end
